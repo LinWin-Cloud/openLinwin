20ed5b5597ac7d046c856cd416c1643363b777cc
LinWin-Cloud <2382546858@qq.com>
2022-10-30 12:58:44 +0800

    proxy/

--- proxy/HttpRequests.java
+++ proxy/HttpRequests.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class HttpRequests {
+import java.net.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class HttpRequests {
+    public static String RequestsHttp(String HTTPurl) throws Exception{
+
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        Future<String> future = executorService.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                URL url = new URL(HTTPurl);
+
+                Socket socket = new Socket(url.getHost(),url.getPort());
+                return null;
+            }
+        });
+        return future.get();
+    }
 }
