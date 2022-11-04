581543d2ead5c2b7b28130c516d4bce9a45b92cb
LinWin-Cloud <96580724+LinWin-Cloud@users.noreply.github.com>
2022-11-04 11:52:30 +0800

    修改桌面配置文件，适配开放麒麟系统

---
+++ Sevice/desktop/alert.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class alert
+{
+	public static void main(String[]args)
+	{
+		JOptionPane.showMessageDialog(null, "Boot Successful [HTTP服务启动成功,在终端内输入: linwinhttp 查看选项]", "成功启动", JOptionPane.WARNING_MESSAGE);
+	}
+}
