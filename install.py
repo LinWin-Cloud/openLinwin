67589ef7043d22c1385cee09d9156b332ee31b6a
LinWin-Cloud <2382546858@qq.com>
2022-12-03 12:57:57 +0800

    修改安装程序

--- install.py
+++ install.py
@@ -14,18 +14,20 @@ def Install():
             os.system('mkdir /usr/LinWinHttp')
             os.system('mkdir /usr/www')
             os.system('mkdir /usr/www/html')
-            os.system('cp -rf * /usr/LinWinHttp/')
-            os.system('chmod +x /usr/LinWinHttp/Sevice/shell/*.sh')
+            os.system('cp -rf ./* /usr/LinWinHttp/')
+            time.sleep(0.1)
+            os.system('chmod +x /usr/LinWinHttp/Sevice/shell/*')
             os.system('cp /usr/LinWinHttp/default/page/linwinhttp-index.html /usr/www/html') 
-            os.system('cd /usr/LinWinHttp/nodeJS && unzip linux.zip')
+            # os.system('cd /usr/LinWinHttp/nodeJS && unzip linux.zip')
             # os.system('cd /usr/LinWinHttp/Sevice/ && ../jre/bin/java -jar ServerBoot.jar')
             os.system('chmod 777 /usr/www/html')
             os.system('cp -f Sevice/systemd/linwinhttp.service /etc/systemd/system/')
             os.system('systemctl daemon-reload')
             os.system('systemctl linwinhttp start')
-            os.system('cp /usr/LinWinHttp/sys/start_linwinhttp /etc/init.d')
+            # os.system('cp /usr/LinWinHttp/sys/start_linwinhttp /etc/init.d')
             os.system('chmod +x /usr/LinWinHttp/* -R')
-            os.system('cd /usr/LinWinHttp/JRE && unzip openJDK8.zip')
+            os.system('cd /usr/LinWinHttp/JRE && tar -xvf  openJDK8.tar.xz')
+            os.system('chmod +x /usr/LinWinHttp/sys/start_linwinhttp')
             os.system('/usr/LinWinHttp/sys/start_linwinhttp')
 
             os.system('cp /usr/LinWinHttp/sys/linwinhttp /bin/')
@@ -37,7 +39,7 @@ def Install():
             os.system('chmod +x /etc/rc.d/rcX.d/linwinhttp')
             os.system('systemctl enable linwinhttp.service')
             os.system('cp -f /usr/LinWinHttp/Sevice/desktop/linwinhttp.desktop /usr/share/applications')
-
+ 
             os.system('cp /usr/LinWinHttp/sys/linwindoc /bin/')
             os.system('chmod +x /bin/linwindoc')
 
@@ -49,9 +51,12 @@ def Install():
 
             os.system('cp /usr/LinWinHttp/sys/linwindoc /bin/ && chmod +x /bin/linwindoc')
             os.system('chmod 777 /usr/LinWinHttp/* -R')
+
+            os.system("cp /usr/LinWinHttp/sys/linwinMulti /bin/")
+            os.system("chmod +x /bin/linwinMulti")
             
             print('[!] Finish Install!Install Path: /usr/LinWinHttp/')
-            print('[!] Input: \"linwinboot\" in the terminal,then start the HTTP Server.Default Port Is 8080')
+            os.system("linwinMulti")
         print('Install ... ...')
         if os.path.exists("/usr/LinWinHttp/") and os.path.isdir("/usr/LinWinHttp/"):
             a()
