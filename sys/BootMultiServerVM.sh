1c5d64297f0ebceff7d89f21a7b9f7ba9d8285a8
LinWin-Cloud <2382546858@qq.com>
2022-12-16 22:10:52 +0800

    多服务器启动脚本

--- sys/BootMultiServerVM.sh
+++ sys/BootMultiServerVM.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-/usr/LinWinHttp/JRE/openJDK8/bin/java -jar ../release/com.linwinMultiServer.package.jar $1 $2
+/usr/LinWinHttp/JRE/openJDK8/bin/java -jar ../release/com.linwinMultiServer.package.jar $1 $2 $3
