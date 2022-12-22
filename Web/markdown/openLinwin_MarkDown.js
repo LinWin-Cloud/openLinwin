98d31c3ab5b02920e70be8cf3985c13b7795e053
LinWin-Cloud <2382546858@qq.com>
2022-12-22 21:14:47 +0800

    前端

---
+++ Web/markdown/openLinwin_MarkDown.js
@@ -0,0 +1,11 @@
+/**
+ * Auther: LinwinCloud
+ * https://github.com/LinWin-Cloud/
+ */
+
+window.onload = function() {
+    var str = document.getElementById('markdown').innerText;
+    
+    if (str.indexOf("** ") != -1) {
+    }
+}
--- com.linwinMultiServer.package/src/ApiKeyService.java
+++
@@ -1,6 +0,0 @@
-
-public class ApiKeyService {
-    public static void main(String[] args) {
-        
-    }
-}
