a055dd3a235b9fe2ee99e1701b646f7a42219e9c
奥利给干了兄弟们 <an2382546858@outlook.com>
2023-01-05 20:31:44 +0800

    更新了开放代理服务器模块的快速调试命令行

--- sys/BootMultiServerVM.sh
+++ sys/BootMultiServerVM.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-/usr/LinWinHttp/JRE/openJDK8/bin/java -jar ../release/com.linwinMultiServer.package.jar $1 $2 $3
+/usr/LinWinHttp/com.linwinMultiServer.package/build.sh
+/usr/LinWinHttp/JRE/openJDK8/bin/java -jar /usr/LinWinHttp/com.linwinMultiServer.package/release/com.linwinMultiServer.package.jar $1 $2 $3
